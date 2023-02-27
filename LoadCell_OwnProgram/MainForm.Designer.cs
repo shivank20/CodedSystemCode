a93a7cfa4e329fec066e9bd20a313ddc9298f38c
shivank20 <79879577+shivank20@users.noreply.github.com>
2023-02-27 16:52:11 -0600

    Add project files.

---
+++ LoadCell_OwnProgram/MainForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace LoadCell_OwnProgram
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ResultsLab = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button3.Location = new System.Drawing.Point(233, 264);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(243, 77);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button4.Location = new System.Drawing.Point(913, 264);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(243, 77);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // ResultsLab
+            // 
+            this.ResultsLab.AutoSize = true;
+            this.ResultsLab.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.6F);
+            this.ResultsLab.Location = new System.Drawing.Point(589, 112);
+            this.ResultsLab.Name = "ResultsLab";
+            this.ResultsLab.Size = new System.Drawing.Size(134, 39);
+            this.ResultsLab.TabIndex = 2;
+            this.ResultsLab.Text = "Results";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.6F);
+            this.label1.Location = new System.Drawing.Point(694, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "lbf";
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(1342, 413);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResultsLab);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label ResultsLab;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ LoadCell_OwnProgram/MainForm.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using FUTEK_USB_DLL;
+using Zaber.Motion;
+using Zaber.Motion.Ascii;
+
+namespace LoadCell_OwnProgram
+{
+    public partial class MainForm : Form
+    {
+        private Thread _calculationThread;
+        private IntPtr DeviceHandle;
+        private int DeviceStatus;
+        private string t_Off_Val;
+        private string t_Full_Val;
+        private string t_FullLoad_Val;
+        private string t_Deci_Point;
+        private string t_NormData;
+        private string t_UnitCode;
+        public Int32 UnitCode;
+        public Int32 OffsetVal;
+        public Int32 FullVal;
+        public Int32 FullLoadVal;
+        public Int32 DeciPoint;
+        public Int32 NormalVal;
+        public Double CalcVal;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //This is to calculate the laod cell force
+        private void Calculate()
+        {
+            FUTEK_USB_DLL.USB_DLL futek = new FUTEK_USB_DLL.USB_DLL();
+            
+
+            // Connect to the device
+            futek.Open_Device_Connection("538827");
+            DeviceStatus = futek.DeviceStatus;
+            if (DeviceStatus == 0)
+            {
+                MessageBox.Show("The device is connected");
+            }
+            DeviceHandle = futek.DeviceHandle;
+
+            while (true)
+            {
+                // Getting the required values
+                t_Off_Val = futek.Get_Offset_Value(DeviceHandle);
+                OffsetVal = Int32.Parse(t_Off_Val);
+                t_Full_Val = futek.Get_Fullscale_Value(DeviceHandle);
+                FullVal = Int32.Parse(t_Full_Val);
+                t_FullLoad_Val = futek.Get_Fullscale_Load(DeviceHandle);
+                FullLoadVal = Int32.Parse(t_FullLoad_Val);
+                t_Deci_Point = futek.Get_Decimal_Point(DeviceHandle);
+                DeciPoint = Int32.Parse(t_Deci_Point);
+                t_NormData = futek.Normal_Data_Request(DeviceHandle);
+                NormalVal = Int32.Parse(t_NormData);
+                t_UnitCode = futek.Get_Unit_Code(DeviceHandle);
+                UnitCode = Int32.Parse(t_UnitCode);
+
+                // Calculate the value
+                CalcVal = (double)(NormalVal - OffsetVal) / (FullVal - OffsetVal) * FullLoadVal / Math.Pow(10, DeciPoint);
+
+                // Display the result
+                if (InvokeRequired)
+                {
+                    Invoke(new Action(() => ResultsLab.Text = CalcVal.ToString("n2")));
+                }
+                else
+                {
+                    ResultsLab.Text = CalcVal.ToString("n2");
+                }
+
+                // Wait for a second before calculating again
+                Thread.Sleep(1000);
+            }
+        }
+
+        //This is to move the linear actuator
+        private void LinearActuator()
+        {
+            using (var connection = Connection.OpenSerialPort("COM5"))
+            {
+                connection.EnableAlerts();
+
+                var deviceList = connection.DetectDevices();
+                Console.WriteLine($"Found {deviceList.Length} devices.");
+
+                var device = deviceList[0];
+
+                var axis = device.GetAxis(1);
+                if (!axis.IsHomed())
+                {
+                    axis.Home();
+                }
+
+                // Move to 10mm
+                axis.MoveAbsolute(10, Units.Length_Millimetres);
+
+                // Move by an additional 5mm
+                axis.MoveRelative(5, Units.Length_Millimetres);
+            }
+        }
+
+
+        //This start the load cell calculation
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Disable the start button
+            button3.Enabled = false;
+
+            // Start the calculation thread
+            _calculationThread = new Thread(Calculate);
+            _calculationThread.Start();
+            MessageBox.Show("About to Start Calculations!");
+
+            //Moving the linear actuator
+            MessageBox.Show("Running the linear Actuator");
+            LinearActuator();
+        }
+        //This stops the load cell calculation
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (_calculationThread != null && _calculationThread.IsAlive)
+            {
+                _calculationThread.Abort();
+            }
+
+            // Enable the start button
+            button3.Enabled = true;
+            this.Close();
+            Application.Exit();
+        }
+    }
+}
